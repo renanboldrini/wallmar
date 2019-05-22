c4d19264577e55b497eb75d7a8428f778dec0248
RENAN FONTOURA BOLDRINI RAMOS <631620025@fspoa.br>
2019-05-22 20:50:15 -0300

    first commit

---
+++ wallmar/Controllers/DepartamentsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using wallmar.Models;
+
+namespace wallmar.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Instância Lista de departamentos
+            List<Departament> list = new List<Departament>();
+            list.Add(new Departament { Id = 1, Name = "brinquedos" });
+            list.Add(new Departament { Id = 2, Name = "alimentação" });
+            list.Add(new Departament { Id = 3, Name = "vestuario" });
+
+            return View(list);
+        }
+    }
+}
---
+++ wallmar/Models/Departament.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wallmar.Models
+{
+    public class Departament
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+
+    }
+}
