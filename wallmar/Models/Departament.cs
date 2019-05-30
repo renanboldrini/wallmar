899c7da8f485fc2ce355d10f33f78e505de553a6
RENAN FONTOURA BOLDRINI RAMOS <631620025@fspoa.br>
2019-05-29 21:48:17 -0300

    segunda aula

--- wallmar/Models/Departament.cs
+++ wallmar/Models/Departament.cs
@@ -9,6 +9,29 @@ namespace wallmar.Models
     {
         public int Id { get; set; }
         public String Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; }
+            = new List<Seller>();
 
+        public Departament(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        /*Add Seller*/
+        public void AddSeler(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            /*vamos utilizar o LINQ para filtrar a lista de vendas com lambda*/
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+
+        }
+
+        /* Metodo Construtor */
+        public Departament() { } 
     }
 }
